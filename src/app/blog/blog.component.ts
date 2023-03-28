5c14fb3b74505ef9cc9bd14b9fc41f7050976126
Einer Bravo <einerbravo@gmail.com>
2023-03-27 23:12:12 -0500

    Add project

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,32 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { CveinerbravoService } from '../services/cveinerbravo.service';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  styleUrls: ['./blog.component.css'],
 })
 export class BlogComponent implements OnInit {
+  constructor(private sanitizer: DomSanitizer) {}
 
-  constructor(public cv: CveinerbravoService,
-              private sanitizer: DomSanitizer
-              ) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   getVideoIframe(url) {
     let video;
     let results;
 
     if (url === null) {
-        return '';
+      return '';
     }
     results = url.match('[\\?&]v=([^&#]*)');
-    video = (results === null) ? url : results[1];
+    video = results === null ? url : results[1];
 
-    return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + video);
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      'https://www.youtube.com/embed/' + video
+    );
   }
-
 }
--- src/app/resumen/resumen.component.ts
+++ src/app/resumen/resumen.component.ts
@@ -1,16 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { CveinerbravoService } from '../services/cveinerbravo.service';
 
 @Component({
   selector: 'app-resumen',
   templateUrl: './resumen.component.html',
-  styleUrls: ['./resumen.component.css']
+  styleUrls: ['./resumen.component.css'],
 })
 export class ResumenComponent implements OnInit {
+  constructor() {}
 
-  constructor(public cv: CveinerbravoService) { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
